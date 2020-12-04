895e68239326714181998c9a73eed285ef242d67
DivadNojnarg <dgranjon@ymail.com>
2020-12-04 17:47:56 +0100

    lint

--- inst/examples/boxes/social_box.R
+++ inst/examples/boxes/social_box.R
@@ -1,24 +1,24 @@
 social_box <- 'socialBox(
-        width = 12,
-          title = userBlock(
-            image = "https://adminlte.io/themes/AdminLTE/dist/img/user4-128x128.jpg",
-            title = "Social Box",
-            subtitle = "example-01.05.2018"
-          ),
-          "Some text here!",
-          attachmentBlock(
-            image = "https://adminlte.io/themes/AdminLTE/dist/img/photo1.png",
-            title = "Test",
-            href = "https://google.com",
-            "This is the content"
-          ),
-          lapply(X = 1:10, FUN = function(i) {
-            boxComment(
-              image = "https://adminlte.io/themes/AdminLTE/dist/img/user3-128x128.jpg",
-              title = paste("Comment", i),
-              date = "01.05.2018",
-              paste0("The ", i, "-th comment")
-            )
-          }),
-          footer = "The footer here!"
-        )'
+  width = 12,
+  title = userBlock(
+    image = "https://adminlte.io/themes/AdminLTE/dist/img/user4-128x128.jpg",
+    title = "Social Box",
+    subtitle = "example-01.05.2018"
+  ),
+  "Some text here!",
+  attachmentBlock(
+    image = "https://adminlte.io/themes/AdminLTE/dist/img/photo1.png",
+    title = "Test",
+    href = "https://google.com",
+    "This is the content"
+  ),
+  lapply(X = 1:10, FUN = function(i) {
+    boxComment(
+      image = "https://adminlte.io/themes/AdminLTE/dist/img/user3-128x128.jpg",
+      title = paste("Comment", i),
+      date = "01.05.2018",
+      paste0("The ", i, "-th comment")
+    )
+  }),
+  footer = "The footer here!"
+)
--- inst/examples/boxes/widget_user_box.R
+++ inst/examples/boxes/widget_user_box.R
@@ -1,42 +1,42 @@
 widget_user_box1 <- 'userBox(
-width = 12,
+  width = 12,
   title = userDescription(
-            title = "Nadia Carmichael",
-            subtitle = "lead Developer",
-            type = 2,
-            image = "https://adminlte.io/themes/AdminLTE/dist/img/user7-128x128.jpg",
-          ),
+    title = "Nadia Carmichael",
+    subtitle = "lead Developer",
+    type = 2,
+    image = "https://adminlte.io/themes/AdminLTE/dist/img/user7-128x128.jpg",
+  ),
   status = "warning",
   "Some text here!",
   footer = "The footer here!"
 )'
 
 widget_user_box2 <- 'userBox(
-width = 12,
-          title = userDescription(
-            title = "Alexander Pierce",
-            subtitle = "Founder & CEO",
-            type = 1,
-            image = "https://adminlte.io/themes/AdminLTE/dist/img/user1-128x128.jpg",
-          ),
-          status = "purple",
-          closable = TRUE,
-          "Some text here!",
-          footer = "The footer here!"
-        )'
+  width = 12,
+  title = userDescription(
+    title = "Alexander Pierce",
+    subtitle = "Founder & CEO",
+    type = 1,
+    image = "https://adminlte.io/themes/AdminLTE/dist/img/user1-128x128.jpg",
+  ),
+  status = "purple",
+  closable = TRUE,
+  "Some text here!",
+  footer = "The footer here!"
+)'
 
 
 widget_user_box3 <- 'userBox(
-width = 12,
-          title = userDescription(
-            title = "Elizabeth Pierce",
-            subtitle = "Web Designer",
-            image = "https://adminlte.io/themes/AdminLTE/dist/img/user3-128x128.jpg",
-            backgroundImage = "https://cdn.statically.io/img/wallpaperaccess.com/full/1119564.jpg",
-          ),
-          status = "teal",
-          closable = TRUE,
-          maximizable = TRUE,
-          "Some text here!",
-          footer = "The footer here!"
-        )'
+  width = 12,
+  title = userDescription(
+    title = "Elizabeth Pierce",
+    subtitle = "Web Designer",
+    image = "https://adminlte.io/themes/AdminLTE/dist/img/user3-128x128.jpg",
+    backgroundImage = "https://cdn.statically.io/img/wallpaperaccess.com/full/1119564.jpg",
+  ),
+  status = "teal",
+  closable = TRUE,
+  maximizable = TRUE,
+  "Some text here!",
+  footer = "The footer here!"
+)'
